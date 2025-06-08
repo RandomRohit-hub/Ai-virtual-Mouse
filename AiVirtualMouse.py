12eb026b53892851eed25cac3e83dc1b854ba17e
Rohit Sharma <fakeeclipx@gmail.com>
2025-06-08 20:31:54 +0530

    Add files via upload

---
+++ AiVirtualMouse.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+import hand_Module as htm
+import time
+import autopy
+
+wCam, hCam = 640, 480
+
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+
+while True:
+    success, img = cap.read()
+
+    cv2.imshow("image", img)
+    cv2.waitKey(1)
