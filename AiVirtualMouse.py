1bc05cca89682e402165103a06bd248370245390
Rohit Sharma <fakeeclipx@gmail.com>
2025-06-13 23:23:06 +0530

    Add files via upload

--- AiVirtualMouse.py
+++ AiVirtualMouse.py
@@ -1,74 +1,73 @@
 import cv2
 import numpy as np
-import hand_Module as htm
+import HandTrackingModule as htm
 import time
 import autopy
 
-# Set camera dimensions
+##########################
 wCam, hCam = 640, 480
-frameR = 100  # Frame reduction for cursor control area
-smootheninng= 5
+frameR = 100  # Frame Reduction
+smoothening = 7
+#########################
 
-# Initialize webcam
-cap = cv2.VideoCapture(0)
+pTime = 0
+plocX, plocY = 0, 0
+clocX, clocY = 0, 0
+
+cap = cv2.VideoCapture(1)
 cap.set(3, wCam)
 cap.set(4, hCam)
-
-pTime = 0  # Previous time for FPS calculation
 detector = htm.handDetector(maxHands=1)
-
-# Get screen size
 wScr, hScr = autopy.screen.size()
-print(wScr, hScr)
+# print(wScr, hScr)
 
 while True:
-# 1. Find hand landmarks
+    # 1. Find hand Landmarks
     success, img = cap.read()
     img = detector.findHands(img)
     lmList, bbox = detector.findPosition(img)
-
- # 2. Get the tip of the index and middle fingers
+    # 2. Get the tip of the index and middle fingers
     if len(lmList) != 0:
-        x1, y1 = lmList[8][1:]  # Index finger tip
-        x2, y2 = lmList[12][1:]  # Middle finger tip
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+        # print(x1, y1, x2, y2)
 
-        print(x1, y1, x2, y2)
+    # 3. Check which fingers are up
+    fingers = detector.fingersUp()
+    # print(fingers)
+    cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),
+                  (255, 0, 255), 2)
+    # 4. Only Index Finger : Moving Mode
+    if fingers[1] == 1 and fingers[2] == 0:
+        # 5. Convert Coordinates
+        x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
+        y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
+        # 6. Smoothen Values
+        clocX = plocX + (x3 - plocX) / smoothening
+        clocY = plocY + (y3 - plocY) / smoothening
 
-  # 3. Check which fingers are up
-        fingers = detector.fingersUp()
+        # 7. Move Mouse
+        autopy.mouse.move(wScr - clocX, clocY)
+        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+        plocX, plocY = clocX, clocY
 
-  # 4. Draw frame rectangle
-        cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (250, 0, 250), 2)
+    # 8. Both Index and middle fingers are up : Clicking Mode
+    if fingers[1] == 1 and fingers[2] == 1:
+        # 9. Find distance between fingers
+        length, img, lineInfo = detector.findDistance(8, 12, img)
+        print(length)
+        # 10. Click mouse if distance short
+        if length < 40:
+            cv2.circle(img, (lineInfo[4], lineInfo[5]),
+                       15, (0, 255, 0), cv2.FILLED)
+            autopy.mouse.click()
 
- # 5. Moving Mode: Only Index Finger Up
-        if fingers[1] == 1 and fingers[2] == 0:
- # 6. Convert coordinates
-            x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
-            y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
-
- # 7. Move mouse
-            autopy.mouse.move(wScr - x3, y3)
-            cv2.circle(img, (x1, y1), 5, (0, 255, 250), cv2.FILLED)
-
-# 8. Clicking Mode: Both Index and Middle Fingers Up
-        if fingers[1] == 1 and fingers[2] == 1:
-            length, img, Lineinfo = detector.findDistance(8, 12, img)  # Fixed call
-            print(length)
-            if length < 20:
-                cv2.circle(img, (Lineinfo[4],Lineinfo[5]), 5, (0, 255, 250), cv2.FILLED)
-                autopy.mouse.click()
-
-# 10. Click mouse if distance is short (threshold example: < 40)
-            if length < 40:
-                autopy.mouse.click()
-
-    # 11. Frame rate calculation
+    # 11. Frame Rate
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-
-    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 2)
-
+    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                (255, 0, 0), 3)
     # 12. Display
     cv2.imshow("Image", img)
     cv2.waitKey(1)
